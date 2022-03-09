11ce258057da23ee14469a8bde2b07eb08b838a8
craigyjp <51026305+craigyjp@users.noreply.github.com>
2022-03-09 14:47:22 +0200

    Version 1.1 with correct mapping to CS80 times and frequencies

---
+++ MidiCC.h
@@ -0,0 +1,131 @@
+//MIDI CC control numbers
+//These broadly follow standard CC assignments
+#define   CCmodwheel  1 //pitch LFO amount - less from mod wheel
+#define CCvolumeControl   7
+
+#define CCmidiSync1 2
+#define CClfoMode1  3
+#define CClfoWave1  4
+#define CClfoSpeed1 5
+#define CCpwm1  6
+#define CCpw1 8
+#define CCsquare1 9
+#define CCsaw1  10    
+#define CCnoise1  12
+#define CCdb241 13
+#define CChpf1  14
+#define CClpf1  15
+#define CChpfCutoff1  16
+#define CChpfRes1 17
+#define CClpfCutoff1  18
+#define CClpfRes1 19
+
+#define CCvcfIL1  20
+#define CCvcfAL1  21
+#define CCvcfAttack1  22
+#define CCvcfDecay1 23
+#define CCvcfRelease1 24
+#define CCvcfLevel1 25
+#define CCosc1Sine  26
+#define CCampAttack1  27
+#define CCampDecay1 28
+#define CCampSustain1 29
+#define CCampRelease1 30
+#define CCvcaLevel1 31
+#define CCinitial1Brill 122
+#define CCinitial1Level 33
+#define CCafter1Brill 34
+#define CCafter1Level 35
+
+#define CCmidiSync2 36
+#define CClfoMode2  37
+#define CClfoWave2  38
+#define CClfoSpeed2 39
+#define CCpwm2  40
+#define CCpw2 41
+#define CCsquare2 42
+#define CCsaw2  43
+#define CCnoise2  44
+#define CCdb242 45
+#define CChpf2  46
+#define CClpf2  47
+#define CChpfCutoff2  48
+#define CChpfRes2 49
+#define CClpfCutoff2  50
+#define CClpfRes2 51
+
+#define CCvcfIL2  52
+#define CCvcfAL2  53
+#define CCvcfAttack2  54
+#define CCvcfDecay2 55
+#define CCvcfRelease2 56
+#define CCvcfLevel2 57
+#define CCosc2Sine  58
+#define CCampAttack2  59
+#define CCampDecay2 60
+#define CCampSustain2 61
+#define CCampRelease2 62
+#define CCvcaLevel2 63
+#define CCinitial2Brill 94
+#define CCinitial2Level 65
+#define CCafter2Brill 66
+#define CCafter2Level 67
+
+#define CCsubWheel  68
+#define CCdetune  69
+#define CCringAttack  70
+#define CCringDecay 71
+#define CCringDepth 72
+#define CCringSpeed 73
+#define CCringMod 74
+#define CCfunction  75
+#define CCsubSpeed  76
+#define CCsubVCO  77
+#define CCsubVCF  78
+#define CCsubVCA  79
+#define CCfeet1 80
+#define CCfeet2 81
+#define CCmix 82
+
+#define CCbrilliance  83
+#define CCreso  84
+#define CCtouchPbend  85
+#define CCtouchSpeed  86
+#define CCtouchVCO  87
+#define CCtouchVCF  88
+#define CCbrillLow  89
+#define CCbrillHigh 90
+#define CClevelLow  91
+#define CClevelHigh 92
+#define CCsync  93
+#define CClink  120
+
+#define CCarpSync 95
+#define CCarpSpeed  96
+#define CCarpPlay 97
+#define CCarpHold 98
+#define CCarpMode 99
+#define CCarpOctave 100
+#define CCarpRepeat 101
+#define CCexpression  102
+#define CCexpwah  103
+#define CCsust  104
+#define CCportaGliss  105
+#define CCsustMode  106
+#define CCgliss 107
+#define CCsustLongShort 108
+#define CCglissLongShort  109
+#define CCchorusSpeed 110
+
+#define CCchorusDepth 111
+#define CCchorusOn  112
+#define CCtremeloOn 113
+#define CCdelaySpeed  114
+#define CCdelayDepth  115
+#define CCdelayMix  116
+#define CCdelayOn 117
+#define CCdelaySync 118
+#define CCribbonPitch 119
+#define CCribbonCourse  121
+
+#define   CCallnotesoff 64//Panic button
